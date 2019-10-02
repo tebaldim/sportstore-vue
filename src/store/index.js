3f651b51d3e9a539a5eed5efd1a79194582a1ac3
Matteo Tebaldi <m.tebaldi@hub-net.it>
2019-10-02 14:58:46 +0200

    fully featured

--- src/store/index.js
+++ src/store/index.js
@@ -3,16 +3,17 @@ import Vuex from "vuex";
 import Axios from "axios";
 import CartModule from "./cart";
 import OrdersModule from "./orders";
+import AuthModule from "./auth";
 
 Vue.use(Vuex);
 
-const baseUrl = "http://localhost:3500";
+const baseUrl = "/api";
 const productsUrl = `${baseUrl}/products`;
 const categoriesUrl = `${baseUrl}/categories`;
 
 export default new Vuex.Store({
-    strict: true,
-    modules:  { cart: CartModule, orders: OrdersModule },
+    strict: false,
+    modules:  { cart: CartModule, orders: OrdersModule, auth: AuthModule },
     state: {
         categoriesData: [],
         currentPage: 1,
@@ -26,7 +27,10 @@ export default new Vuex.Store({
     getters: {
         processedProducts: (state) => { return state.pages[state.currentPage]; },
         pageCount: (state) => state.serverPageCount,
-        categories: state => ["All", ...state.categoriesData]
+        categories: state => ["All", ...state.categoriesData],
+        productById: (state) => (id) => {
+            return state.pages[state.currentPage].find(p => p.id === id);
+        }
     },
     mutations: {
         _setCurrentPage(state, page) {
@@ -61,6 +65,14 @@ export default new Vuex.Store({
         setSearchTerm(state, term) {
             state.searchTerm = term;
             state.currentPage = 1;
+        },
+        _addProduct(state, product) {
+            state.pages[state.currentPage].unshift(product);
+        },
+        _updateProduct(state, product) {
+            let page = state.pages[state.currentPage];
+            let index = page.findIndex(p => p.id == product.id);
+            Vue.set(page, index, product);
         },
     },
     actions: {
@@ -107,6 +119,23 @@ export default new Vuex.Store({
             context.commit("setSearchTerm", "");
             context.commit("clearPages");
             context.dispatch("getPage", 2);
+        },
+        async addProduct(context, product) {
+            let data = (await context.getters.authenticatedAxios.post(productsUrl,
+                product)).data;
+            product.id = data.id;
+            this.commit("_addProduct", product);
+        },
+        async removeProduct(context, product) {
+            await context.getters.authenticatedAxios
+                .delete(`${productsUrl}/${product.id}`);
+            context.commit("clearPages");
+            context.dispatch("getPage", 1);
+        },
+        async updateProduct(context, product) {
+            await context.getters.authenticatedAxios
+                .put(`${productsUrl}/${product.id}`, product);
+            this.commit("_updateProduct", product);
         }
     }
 });
